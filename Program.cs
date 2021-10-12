344c9c6a15b0753c4f703018dc6225cf219f3d54
William-io <capuletos@live.com>
2021-10-12 20:45:15 -0300

    Starting project

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Data.SqlClient;
+
+
+const string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Database=Dapper;Trusted_Connection=True;MultipleActiveResultSets=True;";
+
+using (var connection = new SqlConnection(connectionString))
+{
+    Console.WriteLine("Conectado");
+    connection.Open();
+
+    using (var command = new SqlCommand())
+    {
+        command.Connection = connection;
+        command.CommandType = System.Data.CommandType.Text;
+        command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+        var reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+        }
+    }
+}
+
+
+
