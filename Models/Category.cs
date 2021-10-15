e3c0e424b75e503887cdc16b5e16c7ce41f3f2e7
William-io <capuletos@live.com>
2021-10-15 10:38:18 -0300

    Created UpdateCategory

--- Models/Category.cs
+++ Models/Category.cs
@@ -4,6 +4,11 @@
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
+        public string Url { get; set; }
+        public int Order { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public bool Featured { get; set; }
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,29 +6,76 @@ using Microsoft.Data.SqlClient;
 
 const string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Database=Dapper;Trusted_Connection=True;MultipleActiveResultSets=True;";
 
+
+
 using (var connection = new SqlConnection(connectionString))
 {
     Console.WriteLine("Conectado");
+    ListCategories(connection);
+    //CreateCategory(connection);
+    UpdateCategory(connection);
 
+
+
+}
+
+static void ListCategories(SqlConnection connection)
+{
     var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-    foreach (var category in categories)
+    foreach (var item in categories)
     {
-        Console.WriteLine($"{category.Id} - {category.Title}");
+        Console.WriteLine($"{item.Id} - {item.Title}");
     }
-    //connection.Open();
+}
 
-    //using (var command = new SqlCommand())
-    //{
-    //    command.Connection = connection;
-    //    command.CommandType = System.Data.CommandType.Text;
-    //    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+static void CreateCategory(SqlConnection connection)
+{
+    var category = new Category();
+    category.Id = Guid.NewGuid();
+    category.Title = "Amazon AWS";
+    category.Url = "amazon";
+    category.Description = "Categoria p/ serviço AWS";
+    category.Order = 8;
+    category.Summary = "AWS Cloud";
+    category.Featured = false;
 
-    //    var reader = command.ExecuteReader();
-    //    while (reader.Read())
-    //    {
-    //        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
-    //    }
-    //}
+
+    var insertSql = @"INSERT INTO
+[Category] 
+VALUES(
+@Id, 
+@Title, 
+@Url, 
+@Summary, 
+@Order, 
+@Description, 
+@Featured)";
+
+    var rows = connection.Execute(insertSql, new
+    {
+        category.Id,
+        category.Title,
+        category.Url,
+        category.Summary,
+        category.Order,
+        category.Description,
+        category.Featured
+    });
+    Console.WriteLine($"{rows} linhas inseridas");
+}
+
+
+//Criando o update
+static void UpdateCategory(SqlConnection connection)
+{
+    var updateQuery = "UPDATE [Category] SET [Title]=@title WHERE [Id]=@id";
+    var rows = connection.Execute(updateQuery, new
+    {
+        id = new Guid("ea8059a2-e679-4e74-99b5-e4f0b310fe6f"),
+        title = "Frontend 2021"
+    });
+
+    Console.WriteLine($"{rows} Regristros atualizados!");
 }
 
 
