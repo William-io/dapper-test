a9c34e1c0e10211dbaa21adb88b52b0292220c27
William <capuletos@live.com>
2021-10-16 10:01:07 -0300

    Method transaction

---
+++ Models/CareerItem.cs
@@ -0,0 +1,12 @@
+using System;
+using Dappers.Models;
+
+namespace Dappers.Models
+{
+    public class CareerItem
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public Course Course { get; set; }
+    }
+}
---
+++ Models/Course.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Dappers.Models
+{
+    public class Course
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+    }
+}
